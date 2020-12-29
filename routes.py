b287f71bc306c7bfdfe5d1831acfd37d5e4b4919
SalarC123 <salarwcheema@gmail.com>
2020-12-29 16:29:34 -0600

    option to provide custom script and style sheet names for each route

--- routes.py
+++ routes.py
@@ -7,38 +7,67 @@ app.config.from_object(Config)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', 
+    style_sheet_name = 'static/style.css', 
+    title = 'Study Tool',
+    script_name = './static/index.js'
+    )
 
-@app.route('/todolist', methods=['GET', 'POST'])
+@app.route('/todolist')
 def todolist():
-    form = TodoListForm()
-    if form.validate_on_submit:
-        flash('Your item has been added!')
-    return render_template('todolist.html', form = form)
+    return render_template('todolist.html', 
+    style_sheet_name = 'static/todolist.css', 
+    title = 'To-do List',
+    script_name = './static/todolist.js'
+    )
 
 @app.route('/goals')
 def goals():
-    return render_template('goals.html')
+    return render_template('goals.html',
+    style_sheet_name = 'static/goals.css', 
+    title = 'Goals',
+    script_name = './static/goals.js'
+    )
 
 @app.route('/brainbreaks')
 def brainbreaks():
-    return render_template('brainbreaks.html')
+    return render_template('brainbreaks.html', 
+    style_sheet_name = 'static/brainbreaks.css', 
+    title = 'Brain Breaks',
+    script_name = './static/brainbreaks.js'
+    )
 
 @app.route('/notes')
 def notes():
-    return render_template('notes.html')
+    return render_template('notes.html',
+    style_sheet_name = 'static/notes.css', 
+    title = 'Notes',
+    script_name = './static/notes.js'
+    )
 
 @app.route('/schedule')
 def schedule():
-    return render_template('schedule.html')
+    return render_template('schedule.html', 
+    style_sheet_name = 'static/schedule.css', 
+    title = 'Schedule',
+    script_name = './static/schedule.js'
+    )
 
 @app.route('/flashcards')
 def flashcards():
-    return render_template('flashcards.html')
+    return render_template('flashcards.html', 
+    style_sheet_name = 'static/flashcards.css', 
+    title = 'Flashcards',
+    script_name = './static/flashcards.js'
+    )
 
 @app.route('/tests')
 def tests():
-    return render_template('tests.html')
+    return render_template('tests.html', 
+    style_sheet_name = 'static/tests.css', 
+    title = 'Tests',
+    script_name = './static/tests.js'    
+    )
 
 if __name__ == '__main__':
     app.run()
