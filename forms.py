b6ad2f947142b5bb0fe894e39ba833578c346bf4
SalarC123 <salarwcheema@gmail.com>
2021-01-01 15:17:11 -0600

    removed: forms work with js now

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class TodoListForm(FlaskForm):
-    todotext = StringField('To-do list', validators=[DataRequired()]) 
-    submit = SubmitField('Add')
